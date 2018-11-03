0acc0ec6af282a309529b3ef7850fd7dfff72b93
Gian Luca Dalla Torre <gianluca.dallatorre@me.com>
2018-11-03 16:18:37 +0100

    New function to Copy Files
    Also test is in place

--- files/auth_file_test.go
+++ files/auth_file_test.go
@@ -7,10 +7,17 @@ import (
 )
 
 func TestFileExists(t *testing.T) {
-
 	assert := assert.New(t)
 	fileManager := AuthFileFileManager{}
 
 	assert.True(fileManager.fileExists("/etc/sudoers"))
 	assert.False(fileManager.fileExists("/etc/bashrca"))
+}
+
+func TestCopyFile(t *testing.T) {
+	assert := assert.New(t)
+	fileManager := AuthFileFileManager{}
+
+	assert.Nil(fileManager.copyFile("/etc/localtime", "/tmp/localtime"))
+
 }
--- files/types.go
+++ files/types.go
@@ -1,6 +1,8 @@
 package files
 
 import (
+	"errors"
+	"io"
 	"os"
 
 	"github.com/torre76/pgbpasswd/types"
@@ -14,14 +16,44 @@ type FileManager interface {
 
 	removeFile(fileName string) error
 
+	// copyFile copy a file from source to destination
+	copyFile(srcFileName string, destFileName string) error
+
 	Read(fileName string) ([]types.LoginPassword, error)
 
+	// Write create a file into fylesystem that contains login and hashed password using a specified file format
 	Write(fileName string, elements []types.LoginPassword) error
 }
 
 // baseFileManager is a base implementation for common method used by FileManager
 type baseFileManager struct {
 	FileManager
+}
+
+func (fm *baseFileManager) copyFile(srcFileName string, destFileName string) error {
+	if !fm.fileExists(srcFileName) {
+		return errors.New("Source File does not exists")
+	}
+
+	source, err := os.Open(srcFileName)
+	if err != nil {
+		return err
+	}
+	defer source.Close()
+
+	destination, err := os.Create(destFileName)
+	if err != nil {
+		return err
+	}
+	defer destination.Close()
+
+	bytes, err := io.Copy(destination, source)
+	_ = bytes
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (fm *baseFileManager) fileExists(filename string) bool {
