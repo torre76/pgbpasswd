ef54db4eaf4537f07f4860c99fecd6c5bb60519f
Gian Luca Dalla Torre <gianluca.dallatorre@me.com>
2018-11-03 23:29:10 +0100

    Implemented Read Function for Auth File
    Test is in place for a valid file that will be read.
    Missing tests with invalid files.

--- files/auth_file.go
+++ files/auth_file.go
@@ -1,6 +1,12 @@
 package files
 
 import (
+	"bufio"
+	"errors"
+	"os"
+	"regexp"
+	"strings"
+
 	"github.com/torre76/pgbpasswd/types"
 )
 
@@ -10,7 +16,45 @@ type authFileFileManager struct {
 }
 
 func (fm *authFileFileManager) Read(fileName string) ([]types.LoginPassword, error) {
-	return nil, nil
+	if !fm.fileExists(fileName) {
+		return nil, errors.New("Cannot read from file, does it exists?")
+	}
+
+	file, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+
+	// Ensure File will be closed at the end
+	defer file.Close()
+
+	result := []types.LoginPassword{}
+	fileScanner := bufio.NewScanner(file)
+
+	/* Complex RegExp to find string like
+	 * "login" "password"
+	 * even if
+	 * * "login" has been escaped
+	 * * There are any kind of characters after second quoting
+	 * Logic has been adapted starting from:
+	 * https://stackoverflow.com/questions/249791/regex-for-quoted-string-with-escaping-quotes
+	 */
+	re := regexp.MustCompile(`^-*"([^"\\]*(\\.[^"\\]*)*)"?.*?"(.*?)".*$`)
+
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+		matches := re.FindStringSubmatch(line)
+
+		if len(matches) < 4 {
+			return nil, errors.New("File format is not valid")
+		}
+
+		result = append(result, *types.NewLoginHashedPassword(
+			strings.Replace(matches[1], `\"`, `"`, -1),  // Replace escape if login has been escaped
+			strings.Replace(matches[3], `\"`, `"`, -1))) // Replace escape if hashed password has been escaped
+	}
+
+	return result, nil
 }
 
 func (fm *authFileFileManager) Write(fileName string, elements []types.LoginPassword) error {
--- files/auth_file_test.go
+++ files/auth_file_test.go
@@ -1,6 +1,7 @@
 package files
 
 import (
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -32,4 +33,15 @@ func TestRemoveFile(t *testing.T) {
 	assert.Nil(fileManager.removeFile("/tmp/localtime_to_delete"))
 	assert.False(fileManager.fileExists("/tmp/localtime_to_delete"))
 
+}
+
+func TestRead(t *testing.T) {
+	assert := assert.New(t)
+	fileManager := NewAuthFileFileManager()
+	asset, _ := filepath.Abs("../test_data/users.txt")
+
+	result, _ := fileManager.Read(asset)
+	assert.NotNil(result)
+	assert.True(len(result) == 3)
+	assert.Equal(result[2].Login, `macha_"test"`)
 }
